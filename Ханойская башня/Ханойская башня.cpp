26f7b8accb2b1f9eb4eff2cf65d7fb211f9a87a3
DmitriyIp <91135391+DmitriyIp@users.noreply.github.com>
2022-03-01 18:04:26 +0500

    Add files via upload

---
+++ Ханойская башня/Ханойская башня.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void hanoitower(int n, int first, int second, int third)
+{
+    if (n > 0)
+    {
+        hanoitower(n - 1, first, third, second);
+        cout << first << " => " << second << endl;
+        hanoitower(n - 1, third, second, first);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int n;
+    cout << "Введите количество дисков: ";
+    cin >> n;
+    hanoitower(n, 1, 3, 2);
+    return 0;
+}
