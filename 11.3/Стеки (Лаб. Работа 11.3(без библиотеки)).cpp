9acda3b6caadb7592d4a856ea9cc23d1c123721d
DmitriyIp <91135391+DmitriyIp@users.noreply.github.com>
2022-03-12 14:07:54 +0500

    Add files via upload

---
+++ 11.3/Стеки (Лаб. Работа 11.3(без библиотеки)).cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+using namespace std;
+
+struct steck
+{
+	char data;
+	steck* next;
+};
+
+void add(steck** top, char d)
+{
+	steck* q;
+	q = new steck(); 
+	q->data = d;
+	if (top == NULL) 
+	{ 
+		*top = q; 
+	}
+	else
+	{
+		q->next = *top; 
+		*top = q; 
+	}
+}
+
+void print(steck* top) 
+{ 
+	steck* q = top; 
+	while (q) 
+	{ 
+		cout << q->data << "\t";
+		q = q->next;
+	}
+}
+
+void addElement(steck** top, char l, char nl)
+{
+	steck* q = *top; 
+	while (q != NULL) 
+	{
+		if (q->data == l) 
+		{
+			steck* newel = new steck;
+			newel->data=nl;
+			newel->next = q->next;
+			q->next = newel;
+		}
+		q = q->next;
+	}
+}
+
+int main()
+{
+	int n;
+	char k, l, nl;
+	setlocale(LC_ALL, "Rus");
+	steck* top = NULL;
+	cout << "Введите количество элементов: ";
+	cin >> n;
+	cout << "Введите элементы типа char: " << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> k;
+		add(&top, k);
+	}
+	cout << endl;
+	cout << "Текущий список: " << endl;
+	print(top);
+	cout << endl;
+	cout << "Введите инф. поле элемента, после которого будет добавляться новый: ";
+	cin >> l;
+	cout << "Введите значение нового элемента: ";
+	cin >> nl;
+	addElement(&top, l, nl);
+	cout << "Измененный список: " << endl;
+	print(top);
+	return 0;
+}
---
+++ 11.3/Стеки (Лаб. Работа 11.3(с библиотекой)).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	int n;
+	char k, l, m;
+	setlocale(LC_ALL, "Rus");
+	stack <char> st;
+	cout << "Введите количество элементов: ";
+	cin >> n;
+	cout << "Элемент после которого добавить: ";
+	cin >> l;
+	cout << "Значение нового элемента: ";
+	cin >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> k;
+		if (k == l)
+		{
+			st.push(m);
+			st.push(k);
+		}
+		else
+		{
+			st.push(k);
+		}
+	}
+	while (!st.empty())
+	{
+		cout << st.top();
+		st.pop();
+	}
+	return 0;
+}
