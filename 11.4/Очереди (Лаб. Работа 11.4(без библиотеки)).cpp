c7f5ecf912f232061fcbc9e991260841b0e82e80
DmitriyIp <91135391+DmitriyIp@users.noreply.github.com>
2022-03-13 13:04:41 +0500

    Add files via upload

---
+++ 11.4/Очереди (Лаб. Работа 11.4(без библиотеки)).cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node
+{
+	double data;
+	Node* next;
+};
+
+
+Node* add(int n)
+{
+	double k;
+	Node* first;
+	Node* p, * r;
+	first = new(Node);
+	cout << "Введите элементы типа double:" << endl;
+	cout << ">>";
+	cin >> k;
+	first->data = k;
+	first->next = 0;
+	p = first;
+	for (int i = 0; i < n - 1; i++)
+	{
+		r = new(Node);
+		cout << ">>";
+		cin >> k;
+		r->data = k;
+		r->next = 0;
+		p->next = r;
+		p = r;
+
+	}
+	return first;
+}
+
+void print(Node* first)
+{
+	Node* p = first;
+	while (p != 0)
+	{
+		if (p->data > 0)
+		{
+			cout << p->data << "\t";
+			p = p->next;
+		}
+		else
+		{
+			p=p->next;
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n;
+	cout << "Введите количество элементов в очереди: ";
+	cin >> n;
+	Node* first = add(n);
+	print(first);
+	return 0;
+}
---
+++ 11.4/Очереди (Лаб. Работа 11.4(с библиотекой)).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	int n;
+	double k;
+	setlocale(LC_ALL, "Rus");
+	queue <double> q;
+	cout << "Введите количество элементов в очереди: ";
+	cin >> n;
+	cout << "Введите элементы:" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << ">>";
+		cin >> k;
+		if (k > 0)
+		{
+			q.push(k);
+		}
+	}
+	cout << "Результат:" << endl;
+	if (q.empty()) cout << "Очередь пуста!";
+	while (!q.empty())
+	{
+		cout << q.front() << "\t";
+		q.pop();
+	}
+	return 0;
+}
